23ead1b50e2605ddf53c1a35f7de5bbbceb151fa
sakthinaveenm <sakthinaveenm@gmail.com>
2021-06-05 14:11:15 +0530

    project files

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Basic With Javascript and React</h1>
+      <p>
+        Shaurya sinha , Who Hosted this Bootcamp for 7 days, He Taught us Basic
+        of Javascript function,Datatypes etc.. and React Basics
+      </p>
+    </div>
+  );
+}
+export default Note;
